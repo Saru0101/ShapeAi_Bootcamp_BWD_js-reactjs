5174516122b5e189165b3670ef54349532810b9f
Saru0101 <86555374+Saru0101@users.noreply.github.com>
2021-06-27 16:46:59 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        java script and React.js bootcamp was a wonderfull experience it was
+        well explained to the students.{" "}
+      </p>
+      <h2>Thank you</h2>
+    </div>
+  );
+}
+
+export default Info;
